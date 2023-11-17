4a271259f2f02d31d0fd8ac04be04b07c242a08d
Talley Lambert <talley.lambert@gmail.com>
2023-11-16 19:13:41 -0500

    rename

--- examples/iconify.py
+++ examples/iconify.py
@@ -3,22 +3,22 @@ from qtpy.QtGui import QIcon
 from qtpy.QtWidgets import QApplication, QPushButton
 
 from superqt import QIconifyIcon
-from superqt.utils import PaletteIconEventFilter
+from superqt.utils import IconPaletteEventFilter
 
 app = QApplication([])
 
 btn = QPushButton()
 btn.setCheckable(True)
-f = PaletteIconEventFilter()
+btn.show()
+f = IconPaletteEventFilter()
 btn.installEventFilter(f)
 
 # search https://icon-sets.iconify.design for available icon keys
-ic = QIconifyIcon("bi:bell")
-ic.addKey("bi:alarm-fill", color="red", rotate=90, state=QIcon.State.On)
+ic = QIconifyIcon("bi:alarm")
+ic.addKey("bi:alarm-fill", color="yellow", state=QIcon.State.On)
 
 btn.setIcon(ic)
 btn.setIconSize(QSize(60, 60))
 
-btn.show()
-btn.setStyleSheet("background-color: blue; color: white")
+btn.setStyleSheet("background-color: blue; color: #333")
 app.exec()
